e65e5cb7304f13ecb24ec209043eb0cdf702b5a4
Fengxue Zhang <zfxrex@live.cn>
2023-11-30 16:15:23 -0600

    add demo for training, storing, loading, and predicting

--- src/utils/general.py
+++ src/utils/general.py
@@ -2,6 +2,7 @@ from typing import Any, Tuple, List
 from torchmetrics import MeanSquaredLogError
 import torch
 import tqdm
+import gpytorch
 
 
 
@@ -22,3 +23,42 @@ def posterior(model:Any, test_x: torch.tensor) -> Tuple[torch.tensor]:
     with torch.no_grad(), gpytorch.settings.use_toeplitz(False), gpytorch.settings.fast_pred_var():
         preds = model.model(test_x)
     return preds.mean, preds.variance, preds.confidence_region()[1], preds.confidence_region()[0]
+
+def store_torch_model(model:Any, path:str)->None:
+    '''
+    Store torch model
+    Input:
+        @model: given model
+        @path: path to store
+    '''
+    torch.save(model.state_dict(), path)
+
+def load_torch_model(model:Any, path:str)->None:
+    '''
+    Load torch model
+    Input:
+        @model: given model
+        @path: path to load
+    '''
+    model.load_state_dict(torch.load(path))
+    return model
+
+def store_dk_model(model:Any, path:str)->None:
+    '''
+    Store DK model
+    Input:
+        @model: given model
+        @path: path to store
+    '''
+    store_torch_model(model, path)
+
+def load_dk_model(model:Any, path:str)->None:
+    '''
+    Load DK model
+    Input:
+        @model: given model
+        @path: path to load
+    '''
+    from src.model.dk import GPRegressionModel
+    loaded_model = GPRegressionModel(model.train_inputs[0], model.train_targets, model.likelihood, model.feature_extractor, low_dim=True)
+    return load_torch_model(loaded_model, path)
